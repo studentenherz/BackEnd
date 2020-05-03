5369402d53650fae316e815a5be4f71a7505507f
Michel Romero Rodríguez <michelcenrr@gmail.com>
2020-05-03 02:05:38 -0300

    Channged to Heroku DB

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,16 @@
 <?php
 
-$dbservername = "localhost";
-$dbusername = "root";
-$dbpassword = "";
-$dbname= "test";
+// Local Host
+// $dbservername = "localhost";
+// $dbusername = "root";
+// $dbpassword = "";
+// $dbname= "test";
+
+// Heroku db
+$dbservername = "us-cdbr-iron-east-01.cleardb.net";
+$dbusername = "bda1832a94bdaf";
+$dbpassword = "f96bd5ef";
+$dbname= "heroku_3017ab9bd457826";
+
 
 $conn = new mysqli($dbservername, $dbusername, $dbpassword, $dbname);
--- includes/logout.php
+++ includes/logout.php
@@ -2,18 +2,22 @@
 
 $data = json_decode(file_get_contents('php://input'));
 
-require "db.php";
+if(isset($data->logout)){
+  require "db.php";
 
-$token = $data->token;
+  $token = $data->token;
 
-$sql = "SELECT token, privkey FROM sessions";
-$result = $conn->query($sql);
+  $sql = "SELECT token, privkey FROM sessions";
+  $result = $conn->query($sql);
 
-while($row = $result->fetch_assoc()){
-  if($row["token"] == hash_hmac('sha256', $token, $row["privkey"])){
-    $stmt = $conn->prepare("DELETE FROM sessions WHERE token=?");
-    $stmt->bind_param("s", $row["token"]);
-    $stmt->execute();
-    echo "donde";
+  while($row = $result->fetch_assoc()){
+    if($row["token"] == hash_hmac('sha256', $token, $row["privkey"])){
+      $stmt = $conn->prepare("DELETE FROM sessions WHERE token=?");
+      $stmt->bind_param("s", $row["token"]);
+      $stmt->execute();
+      echo "donde";
+    }
   }
+}else{
+  header("Location: ../../FrontEnd/index.html");
 }
