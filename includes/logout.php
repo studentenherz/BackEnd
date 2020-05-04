2988408b02a00020f692aacb6cd20484fc56a6e8
Michel Romero Rodríguez <michelcenrr@gmail.com>
2020-05-04 16:11:15 -0300

    test other allows

--- includes/logout.php
+++ includes/logout.php
@@ -2,6 +2,8 @@
 
 
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: X-PINGOTHER, Content-Type");
 echo "hey!";
 
 // print_t( $_POST);
