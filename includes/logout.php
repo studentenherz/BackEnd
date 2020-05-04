d8fe754091f3271a85bf22600a377a0d4f9b3cc9
Michel Romero Rodríguez <michelcenrr@gmail.com>
2020-05-04 15:47:42 -0300

    test

--- includes/logout.php
+++ includes/logout.php
@@ -1,24 +1,27 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-$data = json_decode(file_get_contents('php://input'));
+print_t( $_POST);
+echo $_SERVER['HTTP_ORIGIN'];
 
-if(isset($data->logout)){
-  require "db.php";
-
-  $token = $data->token;
-
-  $sql = "SELECT token, privkey FROM sessions";
-  $result = $conn->query($sql);
-
-  while($row = $result->fetch_assoc()){
-    if($row["token"] == hash_hmac('sha256', $token, $row["privkey"])){
-      $stmt = $conn->prepare("DELETE FROM sessions WHERE token=?");
-      $stmt->bind_param("s", $row["token"]);
-      $stmt->execute();
-      exit("done");
-    }
-  }
-}else{
-  header("Location: ".$frontEndUrl. "index.html");
-}
+// header("Access-Control-Allow-Origin: *");
+// $data = json_decode(file_get_contents('php://input'));
+//
+// if(isset($data->logout)){
+//   require "db.php";
+//
+//   $token = $data->token;
+//
+//   $sql = "SELECT token, privkey FROM sessions";
+//   $result = $conn->query($sql);
+//
+//   while($row = $result->fetch_assoc()){
+//     if($row["token"] == hash_hmac('sha256', $token, $row["privkey"])){
+//       $stmt = $conn->prepare("DELETE FROM sessions WHERE token=?");
+//       $stmt->bind_param("s", $row["token"]);
+//       $stmt->execute();
+//       exit("done");
+//     }
+//   }
+// }else{
+//   header("Location: ".$frontEndUrl. "index.html");
+// }
