64ce5faa735b7a069e3edd2c83a5cbd68d4d8d18
Michel Romero Rodríguez <michelcenrr@gmail.com>
2020-05-05 00:07:47 -0300

    GET & HEAD

--- includes/login.php
+++ includes/login.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, HEAD, OPTIONS");
 header("Access-Control-Allow-Headers: X-PINGOTHER, Content-Type");
 
 $data = json_decode(file_get_contents('php://input'));
